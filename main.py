dbe7f2d7d4d8892fdb1633db52496a504b719f46
dusannj <dusan.njegovnaovic95@gmail.com>
2021-04-10 14:55:00 +0200

    Data prepared.

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 from simpleRNN import simplernn
+from IMDbExample import dataprepare as dp
+
 if __name__ == "__main__":
-    simplernn.simplernn_eval()
+
+    #simplernn.simplernn_eval()
+
+    train_data, valid_data, test_data = dp.create_dataset()
+    
